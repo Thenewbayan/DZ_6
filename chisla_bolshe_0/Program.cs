99c6f84a0731856054e685e8122b7e5055642958
Thenewbayan <chemspirit.trade@gmail.com>
2023-03-02 21:00:14 +0300

    ne gotovo!

---
+++ chisla_bolshe_0/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("введите несколько целых чисел через клавишу enter");
+Console.WriteLine("вводите числа сколько угодно раз, если вы хотите остановиться введите слово -КОНЕЦ-");
+int size = array.Length;
+int[] GetArray(int size)
+{
+for (int i = 0; i < size; i++)
+{
+    string Input = Console.ReadLine();
+    if ((Input != "КОНЕЦ") && (int.TryParse(Input, out array[i]))) ;
+    {
+        array[i] = Convert.ToInt32(Input);
+    };
+    else 
+    {
+        Console.WriteLine("вы ввели неверное значение, попробуйте снова");
+    }
+}
+}
+int GetArray(20);
+for (int i = 0; i < array.Length; i++)
+int count=0;
+{
+    if (array[i] > 0)
+    {
+        count = count+1;
+    }
+} 
+Console.WriteLine($"вы ввели {count} положительных чисел");
---
+++ cross_line/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("введите параметры для 1 прямой по формуле y=k*x+b, где к и b вы задаете сами через enter");
+double CoefX1=Convert.ToInt32(Console.ReadLine());//coof K
+double CoefB1=Convert.ToInt32(Console.ReadLine());// coof B
+Console.WriteLine("введите параметры для 2 прямой по формуле y=k*x+b, где к и b вы задаете сами через enter");
+double CoefX2=Convert.ToInt32(Console.ReadLine()); //coof K2
+double CoefB2=Convert.ToInt32(Console.ReadLine());// coof B2
+//решаем через равенство (в пересечении у и х для обоих уравнений равны, значит по у их можно приравнять)
+//K*x+b=K2*x+b2=> K*x-K2*x=b2-b=>x=(b2-b)/(K-K2)
+if ((CoefX1==CoefX2)&&(CoefB1==CoefB2))
+{
+    Console.WriteLine("Наши прямые совпадают и имеют бесконечное число общих точек");
+}
+if ((CoefX1-CoefX2)!=0)
+{
+double coordX=(CoefB2-CoefB1)/(CoefX1-CoefX2);
+double coordY=(CoefX1*coordX)+CoefB1;
+}
+else 
+{
+    Console.WriteLine("прямые не преесекаются");
+}
+//Console.WriteLine($"наши прямые пересекаются в точке  {Math.Round(coordX, 1)},{Math.Round(coordY, 1)}");
+Console.WriteLine(coordX);
+Console.WriteLine(coordY);
