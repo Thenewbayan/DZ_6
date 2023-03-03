1fde16a4c2f587fe0e20c8ce8a881cff4aa2fc51
Thenewbayan <chemspirit.trade@gmail.com>
2023-03-04 00:00:33 +0300

    binary ok

---
+++ binary/Program.cs
@@ -0,0 +1,20 @@
+﻿// перевод в двоичную систему
+//11=>1011 26=>11010 9=>1001
+//
+string BinaryNumber(int number)
+{
+    string bin="";
+    while (number / 2 != 0)
+    {
+        bin = Convert.ToString(number % 2)+bin;
+        number /= 2;
+    }
+    if(number>=1)
+    {
+        bin = Convert.ToString(number % 2)+bin;
+    }
+    return bin;
+}
+Console.WriteLine("введите число");
+int numberDec = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(BinaryNumber(numberDec));
