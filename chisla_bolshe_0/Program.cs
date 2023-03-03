9e04ea7f16147c5330ba025c9ff6ce4935dd10d4
Thenewbayan <chemspirit.trade@gmail.com>
2023-03-03 19:39:42 +0300

    positive numbers ok

--- chisla_bolshe_0/Program.cs
+++ chisla_bolshe_0/Program.cs
@@ -6,30 +6,37 @@
 // 1, -7, 567, 89, 223-> 3
 
 Console.WriteLine("введите несколько целых чисел через клавишу enter");
+Console.WriteLine("укажите сколько чисел вы хотите ввести");
+int size=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("вводите числа сколько угодно раз, если вы хотите остановиться введите слово -КОНЕЦ-");
-int size = array.Length;
 int[] GetArray(int size)
 {
+    int [] array=new int [size];
+    int arraySize=array.Length;
 for (int i = 0; i < size; i++)
 {
     string Input = Console.ReadLine();
-    if ((Input != "КОНЕЦ") && (int.TryParse(Input, out array[i]))) ;
+    if ((Input != "КОНЕЦ") && (int.TryParse(Input, out array[i]))) 
     {
         array[i] = Convert.ToInt32(Input);
-    };
+    }
     else 
     {
         Console.WriteLine("вы ввели неверное значение, попробуйте снова");
     }
 }
+return array;
 }
-int GetArray(20);
-for (int i = 0; i < array.Length; i++)
+int[] resultArray=GetArray(size);
+Console.WriteLine(String.Join(";", resultArray));
 int count=0;
 {
-    if (array[i] > 0)
+    for (int i=0; i<resultArray.Length; i++)
     {
+        if (resultArray[i]>0)
+        {
         count = count+1;
+        }
     }
 } 
 Console.WriteLine($"вы ввели {count} положительных чисел");
